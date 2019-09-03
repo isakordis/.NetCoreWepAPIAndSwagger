90f99faca5dcb2565b7d59642474183790caeb80
isakordis <isa.kordis02@gmail.com>
2019-09-03 04:00:38 +0300

    UnitTest and IntegrationTest

---
+++ WebAPIExample/UnitTest/DbContextMocker.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAPIExample.Models;
+
+namespace UnitTest
+{
+    public static class DbContextMocker
+    {
+        public static WebAPIContext GetWideWorldImportersDbContext(string dbName)
+        {
+            // Create options for DbContext instance
+            var options = new DbContextOptionsBuilder<WebAPIContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            // Create instance of DbContext
+            var dbContext = new WebAPIContext(options);
+
+            // Add entities in memory
+            dbContext.Seed();
+
+            return dbContext;
+        }
+    }
+}
